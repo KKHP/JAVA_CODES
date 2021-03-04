8bb1ba4081a73c89268659b1d8a44ec7bf5f300e
S A HARIPRASAD <67941813+SAHARIPRASAD-2907@users.noreply.github.com>
2021-03-04 13:51:33 +0530

    commonArrayElements

---
+++ HARI-DA/da1/commonArrayElements.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+class commonArrayElements {
+
+
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int n=0,m=0;
+        String[] a = new String[10];
+        String[] b = new String[10];
+        System.out.println("Enter number of elements in array 1");
+        n=sc.nextInt();
+        System.out.println("Enter number of elements in array 2");
+        m=sc.nextInt();
+        System.out.println("Enter "+ n +" elements for 1st array");
+        for(int i=0;i<n;i++){
+            a[i]=sc.next();
+        }
+        System.out.println("Enter "+ m +" elements for 2nd array");
+        for(int i=0;i<m;i++){
+            b[i]= sc.next();
+
+        }
+
+        FindCommonElementsInArray(a,b, a.length);
+        sc.close();
+
+
+    }
+
+    public static void FindCommonElementsInArray(String[] a,String[] b,int n){
+        System.out.println("Common elements are");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(a[i].equals(b[j])) {
+                    System.out.println(b[j]);
+                    break;
+                }
+
+            }
+        }
+
+
+    }
+
+
+}
+
+
+// output 
+// Enter number of elements in array 1
+// 2
+// Enter number of elements in array 2
+// 2
+// Enter 2 elements for 1st array
+// a
+// b
+// Enter 2 elements for 2nd array
+// a
+// c
+// Common elements are
+// a
