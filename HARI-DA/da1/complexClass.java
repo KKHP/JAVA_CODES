0535889357286693ac92a4f4e5126b180c55dffd
N Kishore Kumar <70754470+N-Kishore-Kumar@users.noreply.github.com>
2021-03-08 17:40:58 +0530

    Update complexClass.java

--- HARI-DA/da1/complexClass.java
+++ HARI-DA/da1/complexClass.java
@@ -1,7 +1,31 @@
-//question 5
-public class complexClass {
-    public static void main(String[] args) {
-        
+class Complex{
+    int r;
+    int i;
+    Complex(){
+        r=1;
+        i=5;
+    }
+    Complex(int i, int r){
+        this.i = i;
+        this.r = r;
+    }
+    Complex(Complex c){
+        i = c.i;
+        r = c.r;
+    }
+    Complex ComplexAdd(Complex c1, Complex c2){
+        Complex c3 = new Complex();
+        c3.r = c2.r + c1.r;
+        c3.i = c2.i + c1.i;
+        return c3;
     }
 }
-
+public class ComplexNumbers {
+    public static void main(String[] args){
+        Complex c1 = new Complex();
+        Complex c2 = new Complex(5,5);
+        Complex c3 = new Complex();
+        c3 = c3.ComplexAdd(c1,c2);
+        System.out.println(c3.r + " + " + c3.i + "i");
+    }
+}
